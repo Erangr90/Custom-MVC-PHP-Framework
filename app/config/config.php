a7a6fd5f4b9d45f5922da17fb6482fd13fd5d728
Eran Grady <erangrady3.0@gmail.com>
2021-11-24 17:25:02 +0200

    Publish

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+    // DB params
+    define('DB_HOST','__Your_host_name/localhost__');
+    define('DB_USER','__Your_user/root__');
+    define('DB_PASS','__Your_password__');
+    define('DB_NAME','__Your_DB_Name__');
+
+    // App root
+    define('APP_ROOT',dirname(__FILE__,2));
+
+    // Url root
+    define('URL_ROOT','__Your_URL__');
+
+    // site Name
+    define('SITE_NAME','__Your_website_name__');
+
+?>
